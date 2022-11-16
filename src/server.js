31fb082e818ce157e01215ce8bd1f67ef8127fb6
kknni <gasiorowicz10@gmail.com>
2022-11-16 22:15:22 +0900

    Add socket definition

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from 'express'; // 최신 자바 문법 express 서버에서 가져오기
 
 const app = express();
@@ -8,7 +10,16 @@ app.set("views", __dirname + "/views"); //__dirname은 Node.js 전역변수로, 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home")); // 사용자에게 home.pug를 제공하기 위한 라우팅 설정
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/*", (req, res) => res.redirect("/")); // 홈(/)이 아닌 다른 주소로 GET요청을 보내더라도 홈으로 리다이렉션 하도록 설정
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket){
+  console.log(socket)
+}
+
+wss.on("connection", handleConnection)
+
+server.listen(3000, handleListen);
