4cdac12725f98df498cf221f047c6044743a2746
kknni <gasiorowicz10@gmail.com>
2022-11-16 22:48:26 +0900

    Add arising event based on protocol

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,21 @@
 // alert("hi!");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 연결 성립
+socket.addEventListener("open", () => {
+  console.log("Connected to Server");
+})
+
+// 메시지가 전달되었을 때
+socket.addEventListener("message", (message) => {
+  console.log("Just got this:", message.data, "from the server"); 
+})
+
+// 서버가 오프라인이 되었을 때, 연결 해제
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server");
+})
+
+setTimeout(() => {
+  socket.send("hello from browser after 5s");
+}, 5000); // 5000ms 뒤에 메시지 보냄
--- src/server.js
+++ src/server.js
@@ -6,9 +6,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views"); //__dirname은 Node.js 전역변수로, 현재 실행하는 폴더의 경로를 의미
-
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => res.render("home")); // 사용자에게 home.pug를 제공하기 위한 라우팅 설정
 app.get("/*", (req, res) => res.redirect("/")); // 홈(/)이 아닌 다른 주소로 GET요청을 보내더라도 홈으로 리다이렉션 하도록 설정
 
@@ -16,10 +14,18 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket){
-  console.log(socket) // socket 내용이 출력되면, connection 이벤트가 발생한 것.
-}
+// function handleConnection(socket){
+//   console.log(socket) // socket 내용이 출력되면, connection 이벤트가 발생한 것.
+// }
+// wss.on("connection", handleConnection)  // socket 확인하려면 connection 이벤트가 발생해야함
 
-wss.on("connection", handleConnection)  // socket 확인하려면 connection 이벤트가 발생해야함
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from Browser"));
+  socket.on("message", (message) => {
+    console.log(`${message}`)
+  })
+  socket.send("hello!!");
+})
 
 server.listen(3000, handleListen);
